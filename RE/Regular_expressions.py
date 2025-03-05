186e438479252f22aafe53e37ad8165c7769683a
Roman <Sapunovrm@bk.ru>
2025-03-06 00:01:34 +0500

    RE

---
+++ RE/Regular_expressions.py
@@ -0,0 +1,63 @@
+import csv
+import re
+from pprint import pprint
+
+# Чтение адресной книги
+with open('phonebook_raw.csv', encoding='utf-8') as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+
+
+# Функция для приведения ФИО в порядок
+def fix_names(contacts_list):
+    fixed_contacts = []
+    for contact in contacts_list:
+        full_name = ' '.join(contact[:3]).split(' ')
+        fixed_name = full_name[:3] + contact[3:]
+        fixed_contacts.append(fixed_name)
+    return fixed_contacts
+
+
+# Функция для приведения телефонов к единому формату
+def fix_phones(contacts_list):
+    phone_pattern = re.compile(
+        r'(\+7|8)\s*\(?(\d{3})\)?[\s-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})\s*(\(?(доб\.?)\s*(\d+)\)?)?'
+    )
+    for contact in contacts_list:
+        phone = contact[5]
+        if phone:
+            match = phone_pattern.search(phone)
+            if match:
+                formatted_phone = f"+7({match.group(2)}){match.group(3)}-{match.group(4)}-{match.group(5)}"
+                if match.group(7) and match.group(8):
+                    formatted_phone += f" доб.{match.group(8)}"
+                contact[5] = formatted_phone
+    return contacts_list
+
+
+# Функция для объединения дублирующихся записей
+def merge_duplicates(contacts_list):
+    merged_contacts = {}
+    for contact in contacts_list:
+        key = (contact[0], contact[1])
+        if key in merged_contacts:
+            for i in range(len(contact)):
+                if not merged_contacts[key][i] and contact[i]:
+                    merged_contacts[key][i] = contact[i]
+        else:
+            merged_contacts[key] = contact
+    return list(merged_contacts.values())
+
+
+# Применяем функции
+contacts_list = fix_names(contacts_list)
+contacts_list = fix_phones(contacts_list)
+contacts_list = merge_duplicates(contacts_list)
+
+# Сохранение результата в новый файл
+with open("phonebook.csv", "w", encoding="utf-8") as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(contacts_list)
+
+print(len(contacts_list) - 1)
+pprint(contacts_list)
---
+++ RE/infa.md
@@ -0,0 +1,7 @@
+https://docs.python.org/3/library/re.html
+https://regex101.com/r/dFQEU2/1
+
+\w+-?\w*
+"(8|\+7)\s*\(\d+\)\s*\d+[\s-]*\d+[\s-]*\d+"gm 
++7(\2)\3\4\5 
+(8|\+7)?\s*\((\d+)\)\s*(\d+)[\s-]*(\d+)[\s-]*(\d+)
